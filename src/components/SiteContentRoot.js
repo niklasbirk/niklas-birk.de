ceb49f9d03fcb8282805cdcf7c0281a3e653ff2e
Niklas <niklas.birk.nb@gmail.com>
2022-09-17 15:50:58 +0200

    Add space between main elements of the site inside a container

--- src/components/SiteContentRoot.js
+++ src/components/SiteContentRoot.js
@@ -1,14 +1,19 @@
 import Services from "./Services";
 import Header from "./Header";
 import Footer from "./Footer";
-
+import {Container, Spacer} from "@nextui-org/react";
 
 function SiteContentRoot() {
     return (
         <>
-            <Header />
-            <Services />
-            <Footer />
+            <Container>
+                <Spacer y={2}/>
+                <Header />
+                <Spacer y={1}/>
+                <Services />
+                <Spacer y={2}/>
+                <Footer />
+            </Container>
         </>
     );
 }
