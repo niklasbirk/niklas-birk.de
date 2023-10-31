1282cde58d9e81206e59aaaa3c055ed353287fdc
Niklas Birk <niklas.birk@niklas-birk.de>
2023-10-31 21:08:35 +0100

    Update link from blog to math

--- src/components/Services.js
+++ src/components/Services.js
@@ -22,27 +22,9 @@ function Services() {
                     vendorText="über Mathematik"
                     logo={blogLogo}
                     buttonText="Zum Blog"
-                    url='https://blog.niklas-birk.de' />
+                    url='https://math.niklas-birk.de' />
             </Grid>
         </Grid.Container>
-
-        // <div id="niklas-birk-root">
-        //     <img id="logo" alt="Ein Logo von Niklas Birk" src={logo}/>
-        //     <div>
-        //         <h1>Work in Progress</h1>
-        //         Das ist eine Seite im Aufbau.
-        //         <br/><br/>
-        //         Wichtige links:
-        //         <ul>
-        //             <li> Mail an <a href="mailto://niklas.birk@niklas-birk.de"> niklas.birk@niklas-birk.de </a></li>
-        //             <li> Meine self-hosted git repositories bei <a href="https://git.niklas-birk.de">Gitea</a></li>
-        //             <li> Mein self-hosted <a href="https://blog.niklas-birk.de">Blog</a></li>
-        //             <li> Meine self-hosted <a href="https://survey.niklas-birk.de">Umfragen</a></li>
-        //             <li> ...</li>
-        //         </ul>
-        //     </div>
-        //     <a href={process.env.PUBLIC_URL + 'impressum.html'}>Impressum und Datenschutz</a>
-        // </div>
     );
 }
 
